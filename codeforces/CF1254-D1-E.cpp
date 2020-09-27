6ba4d661bb67aec6ee62dc67cda80eec8cc500c3
Cynthia <cynthiad@mit.edu>
2020-09-27 19:06:14 -0400

    more

--- codeforces/CF1254-D1-E.cpp
+++ codeforces/CF1254-D1-E.cpp
@@ -3,20 +3,13 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> ii;
 
-const int MXN = 5e5;
+const int MXN = 5e5 + 5;
 ll MOD = 1e9 + 7;
 bool debug = false;
 
-int in[MXN], out[MXN], N;
+int N, in[MXN], out[MXN], numComp[MXN], p[MXN], r[MXN];
 vector<int> adj[MXN];
-ll dp[MXN], dpIn[MXN], dpOut[MXN], f[MXN], ans;
-int numComp[MXN], inCycle[MXN];
-int p[MXN], r[MXN];
-
-void prepare() {
-    f[0] = 1;
-    for (ll i = 1; i < MXN; i++) f[i] = (f[i - 1] * i) % MOD;
-}
+ll dpIn[MXN], dpOut[MXN], f[MXN], ans = 1;
 
 void bad() {
     cout << 0 << endl;
@@ -40,76 +33,74 @@ void unionSet(int i, int j) {
 
 void tryUnion(int a, int b, int u) {
     if (debug) cout << "union " << a << " " << b << endl;
-    int a1 = findSet(a), b1 = findSet(b), c = findSet(u);
-    if (a1 == b1 && a1 != c) bad();
-    if (a1 == b1 && a1 == c) inCycle[u] = 1;
-    if (a1 != b1) numComp[u]--, unionSet(a1, b1);
+    if (findSet(a) == findSet(b)) bad();
+    numComp[u]--, unionSet(a, b);
 }
 
-void dfs(int u, int p = -1) {
-    dp[u] = 1, numComp[u] = 1;
+void dfs(int u, int par = -1) {
+    numComp[u] = (par != -1 ? 1 : 0);
     map<int, int> inM, outM;
 
     //Collect values
     if (in[u]) inM[in[u]] = u;
     if (out[u]) outM[u] = u;
     for (int v : adj[u]) {
-        if (v == p) continue;
+        if (v == par) continue;
         numComp[u]++;
         dfs(v, u);
         if (dpIn[v]) inM[dpIn[v]] = v;
         if (dpOut[v]) outM[dpOut[v]] = v;
-        dp[u] = (dp[u] * dp[v]) % MOD;
     }
 
     //Check for external in/outs
     for (auto p : inM) {
         if (outM.count(p.first)) continue;
         if (dpIn[u]) bad();
-        dpIn[u] = p.first;
+        dpIn[u] = p.first, outM[p.first] = par;
     }
     for (auto p : outM) {
         if (inM.count(p.first)) continue;
         if (dpOut[u]) bad();
-        dpOut[u] = p.first;
+        dpOut[u] = p.first, inM[p.first] = par;
     }
 
     //Union paths
     for (auto p : outM) {
-        if (!inM.count(p.first)) continue;
+        if (inM[p.first] == u || p.second == u) continue;
         tryUnion(inM[p.first], p.second, u);
     }
-    if (dpIn[u] && dpOut[u]) tryUnion(inM[dpIn[u]], outM[dpOut[u]], u);
-    if (inCycle[u] && numComp[u] > 1) bad();
-    if (inCycle[u] && u != 1 && !(dpIn[u] || dpOut[u])) bad();
+    if (in[u] && out[u]) {
+        int a = findSet(outM[in[u]]), b = findSet(inM[u]);
+        if (a == b && numComp[u] > 1) bad();
+        if (a != b) tryUnion(inM[u], outM[in[u]], u);
+    }
 
     //Get answer
-    dp[u] = (dp[u] * f[numComp[u] - 1]) % MOD;
-    if (u == 1) ans = dp[u];
-    if (!dpIn[u] && !dpOut[u]) dp[u] = (dp[u] * (numComp[u])) % MOD;
-    if (debug) cout << "dp " << u << " " << dp[u] << endl;
+    ans = (ans * f[numComp[u] - (in[u] || out[u])]) % MOD;
+
+    if (debug) cout << "dp " << u << " " << f[numComp[u] - (in[u] || out[u])] << endl;
     if (debug) cout << "in/out = " << u << " " << dpIn[u] << " " << dpOut[u] << endl;
 }
 
 int main() {
     ios_base::sync_with_stdio(false), cin.tie(0);
 
-    prepare();
+    f[0] = 1;
+    for (ll i = 1; i < MXN; i++) f[i] = (f[i - 1] * i) % MOD;
 
     cin >> N;
     for (int i = 1; i <= N; i++) p[i] = i;
     for (int i = 0, u, v; i < N - 1; i++) {
         cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+        adj[u].push_back(v), adj[v].push_back(u);
     }
     for (int i = 1; i <= N; i++) {
         cin >> in[i];
         if (in[i] == i) bad();
         out[in[i]] = i;
     }
-    dfs(1);
 
+    dfs(1);
     cout << ans << "\n";
 
     return 0;
